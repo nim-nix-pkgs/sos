acb2b248b659b8cb75881562877b9a1428a28634
hugosenari <hugosenari@gmail.com>
2022-09-03 19:18:08 -0300

    chore: re index sos

---
+++ source/README.md
@@ -0,0 +1,207 @@
+<!-- Copyright (c) 2022 Christopher Taylor                                          -->
+<!--                                                                                -->
+<!--   Distributed under the Boost Software License, Version 1.0. (See accompanying -->
+<!--   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        -->
+# [nim-sos - Nim Sandia OpenSHMEM](https://github.com/ct-clmsn/nim-sos)
+
+`nim-sos` wraps the existing [SOS](https://github.com/Sandia-OpenSHMEM/SOS) OpenSHMEM library implemented by Sandia National Laboratory. `nim-sos` provides the [Nim](https://nim-lang.org) programming language distributed symmetric shared memory and Partitioned Global Address Space (PGAS) support.
+
+`nim-sos` provides a *symmetric array type* and a *symmetric scalar type*.
+
+Symmetric arrays are an extension to the existing [Nim array](https://nim-lang.org/docs/manual.html#types-array-and-sequence-types). Symmetric arrays only support values that are of [SomeNumber](https://nim-lang.org/docs/system.html#SomeNumber) types. Symmetric arrays provide element-access, slice, iterator, and partitioning support. Symmetric arrays cannot be appended to; 'add' or 'append' functionality breaks the symmetry property. Symmetric arrays are distributed and globally addressable. Symmetric arrays created with a size as part of their type are called 'symmetric static arrays'. Symmetric arrays created on the heap w/`newSymArray` are simply called symmetric arrays.
+
+Symmetric scalars are globally addressable scalar values.
+
+## SymmetricMain
+
+The `SymmetricMain` macro detects symmetric array and symmetric scalar types during compilation and exposes them to OpenSHMEM. The `SymmetricMain` macro is a *one-stop-shop* for users. *Use of the `SymmetricMain` is mandatory*, the macro finds all symmetric types in a user's program, exposes them to OpenSHMEM, initializes and finalizes OpenSHMEM. The `SymmetricMain` macro can be used as a pragma `{.SymmetricMain.}`.
+
+```
+SymmetricMain:
+    var a = newSymArray[int]([1,2,3,4,5])
+    var b = newSymArray[int](a.len)
+
+    # pick an op to reduce
+    #
+    let rmin = reduce(minop, WORLD, b, a)
+    echo(rmin)
+```
+
+```
+proc main() {.SymmetricMain.} =
+    var a = newSymArray[int]([1,2,3,4,5])
+    var b = newSymArray[int](a.len)
+
+    # pick an op to reduce
+    #
+    let rmin = reduce(minop, WORLD, b, a)
+    echo(rmin)
+
+main()
+```
+
+
+
+## Symmetric Arrays
+
+`nim-sos` provides a *symmetric* version of `array` data types in the tradition of [Fortran Coarrays](https://en.wikipedia.org/wiki/Coarray_Fortran). Symmetric arrays, instantiated by processing elements (PEs[1]) running in SPMD, create a global sequence partitioned across the available PEs. Symmetric arrays consist of globally addressable partitions. Users can `get` from and `put` into a remote partition of the symmetric array.
+
+[1] A PE is a program process running in SPMD on a computer or set of computers. Applications running in SPMD can run in a distributed (cluster) or a single machine setting.
+
+Consider the Symmetric array `S` that is created in an SPMD program running on 2 PEs. `S` spans 2 PEs, or 2 processes residing on the same or a different machine.
+
+```
+        ---------------------------
+        -            S            -
+        -  ++++++++     ++++++++  -
+        -  + PE 0 +     + PE 1 +  -
+        -  +      +     +      +  -
+        -  +  A   +     +  B   +  -
+        -  ++++++++     ++++++++  -
+        ---------------------------
+```
+
+`S` is composed of two partitions, `A` and `B`. `A` resides in the 1st processes memory (PE 0) and `B` resides in a 2nd processes memory (PE 1). The process that contains partition `A` can 'get' a copy of the values in partition `B` using Symmetric array `S` as the shared point of reference. The process that contains partition `B` can 'put' values into partition `A` using the Symmetric array `S` as a shared point of reference. Symmetric array operations are single-sided. PE 0 receives no notifications in the event partition `A` is modified due to a communication operation.
+
+Users are required to define the size of each partition when creating a Symmetric array. Calling the constructor `newSymArray[int](100)` for a 32 node program run will create a Symmetric array with 32 partitions, each partition being 100 integers in type and length. A convenience function called `partitioner` is provided to calculate a partition size given the global number of elements that need to be stored. If a user needs a Symmetric array stored on 32 nodes for 3200 integers, `partitioner` will perform the simple calculation and return 100 integers for each partition.
+
+
+Symmetric arrays can be instantiated either at compile-time or at runtime. Compile-time (static) symmetric arrays are called `symmetric static arrays`. Runtime (dynamic) symmetric arrays are called `symmetric arrays`. The naming convention is used to differentiate the memory allocation used to instatiate the array. To create a compile-time symmetric array, utilize the following type `symindexarray[A, B]` where `A` is an integer value denoting the size of the array to create at compile time and `B` is of `SomeNumber` type.
+
+```
+SymmetricMain:
+   var a : symsarray[100, int]
+   var b : symsarray[500, float64]
+```
+
+The symmetric static array `a` is of type `int` and is 100 elements. The symmetric static array `b` is of type `float64` and is 500 elements.
+
+Arrays declared with the `symarray[T:SomeNumber]` type are created dynamically at runtime. 
+
+```
+SymmetricMain:
+    var a = newSymArray[int]([1,2,3,4,5])
+    var b = newSymArray[int](a.len)
+```
+
+The symmetric array `a` is of type `int` and is 5 elements with values (1,2,3,4,5). The symmetric array `b` is of type `int` and is 5 elements of uninitialized values.
+
+## Symmetric Scalars
+
+Similar to the symmetric array, except for scalar values.
+
+```
+        ---------------------------
+        -            S            -
+        -  ++++++++     ++++++++  -
+        -  + PE 0 +     + PE 1 +  -
+        -  +      +     +      +  -
+        -  +  A   +     +  B   +  -
+        -  ++++++++     ++++++++  -
+        ---------------------------
+```
+
+The scalar value `S` is partitioned across 2 PEs. PE 0 has a scalar value `A`. PE 1 has a scalar value `B`. PE 0 can access `B` on PE 1 using the `S` scalar as a point of reference. PE 1 can access `A` on PE 0 using the `S` scalar as a point of reference.
+
+Symmetric scalar values are declared using the following types:
+
+* `symscalar[T:SomeNumber]`
+* `symint`, `symint8`, `symint16`, `symint32`, `symint64`
+* `symuint`, `symuint8`, `symuint16`, `symuint32`, `symuint64`
+* `symfloat`, `symfloat32`, `symfloat64`
+
+```
+SymmetricMain:
+   var z : symscalar[int]
+   var a : symint
+   var b : symfloat
+   var
+      cee : symint
+      d : symfloat
+```
+
+Symmetric scalars do not support the following operators `+`, `-`, `*`, `=`. Procedures have been implemented to provide support for these operators. All Symmetric scalars have the following methods:
+
+* `add` : add (sum, `+`)
+* `sub` : subtract (difference, `-`)
+* `mul` : multiply (`*`)
+* `sto` : store into the local value (`=`); similar to [atomics](https://nim-lang.org/docs/atomics.html)
+* `read` : get the local value; similar to [atomics](https://nim-lang.org/docs/atomics.html)
+
+Symmetric scalar integers have the following additional operators:
+
+* `div` : integer divide
+* `mod` : integer modulo
+
+Symmetric scalar floats have the following additional operator:
+
+* `div` : integer divide
+* `/` : floating point divide
+
+### Developer Notes
+
+New users are encouraged to review the OpenSHMEM specification [here](http://openshmem.org/site/Specification). Programs
+implemented using `nim-sos` will require use of the [SPMD style](https://en.wikipedia.org/wiki/SPMD).
+
+### Install
+
+Download and install [SOS](https://github.com/Sandia-OpenSHMEM/SOS)
+```
+$ ./configure --prefix=<PATH_TO_INSTALL_DIR> --enable-pmi-simple --disable-threads --disable-openmp --with-oshrun-launcher
+
+$ make && make install
+
+$ export LD_LIBRARY_PATH=<PATH_TO_INSTALL_DIR>/lib:$LD_LIBRARY_PATH
+```
+
+Modify `makefile` to point `LIBDIR` and `INCDIR` to the
+path set in `<PATH_TO_INSTALL_DIR>`. Use the makefile to
+see if your setup compiles.
+```
+make
+```
+
+Use the nimble tool to install `sos`
+```
+nimble install sos
+```
+
+Generate documentation from source
+```
+nimble doc sos
+```
+
+### Running Programs
+
+```
+oshrun -n2 --mpi=pmi2 <program_name>
+```
+
+This library is designed to be run on an HPC system that manages jobs using the following workload managers: [Slurm](https://slurm.schedmd.com), PBS, etc.
+
+### Examples
+
+The directory 'tests/' provides several examples regarding how to utilize this library.
+
+### Licenses
+
+* Boost Version 1.0 (2022-)
+
+### Date
+
+09 March 2022
+
+### Author
+
+Christopher Taylor
+
+### Special Thanks
+
+* The OpenSHMEM developers
+* Sandia National Labs/US Department of Energy
+* The Nim community and user/developer forum
+
+### Dependencies
+
+* [nim 1.6.4](https://nim-lang.org)
+* [Sandia OpenSHMEM](https://github.com/pnnl/rofi)
---
+++ source/sos/bindings.nim
@@ -0,0 +1,198 @@
+#  Copyright (c) 2022 Christopher Taylor
+#
+#  SPDX-License-Identifier: BSL-1.0
+#  Distributed under the Boost Software License, Version 1.0. *(See accompanying
+#  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+{.deadCodeElim: on.}
+#{.passL: "-lsma -lpmi2 -lnss -pthread -lrt ".}
+import std/macros
+
+{.emit: """#include<shmem-def.h>""".}
+
+type Team* {.importc: "shmem_team_t", header: "<shmem-def.h>".} = object
+type Context* {.importc: "shmem_context_t", header: "<shmem.h>".} = object
+type Config* {.importc: "shmem_config_t", header: "<shmem.h>".} = object
+
+{.emit: """extern shmem_team_t SHMEM_TEAM_WORLD; extern shmem_team_t SHMEM_TEAM_SHARED;""".}
+
+var TEAM_WORLD* {.importc: "SHMEM_TEAM_WORLD", header: "<shmem.h>".} : Team
+var TEAM_SHARED* {.importc: "SHMEM_TEAM_SHARED", header: "<shmem.h>".} : Team
+
+proc my_pe*(team : Team) : int {.importc: "shmem_team_my_pe", header : "<shmem.h>".}
+proc n_pes*(team : Team) : int {.importc: "shmem_team_n_pes", header : "<shmem.h>".}
+
+proc translate_pe*(src_team : Team, src_pe : int, dst_team : Team) : int {.importc: "shmem_team_translate_pe", header : "<shmem.h>".}
+proc split_strided*(par_team : Team, strt : int, stride : int, size : int, config : Config, config_mask : int, new_team : var Team) : int {.importc: "shmem_team_split_strided", header : "<shmem.h>".}
+proc split_2d*(par_team : Team, xrng: int, xaxis_config : Config, x_mask : int, xteam : var Team, yaxis_config : Config, y_mask : int, yteam : var Team) : int {.importc: "shmem_team_split_2d", header : "<shmem.h>".}
+proc destroy*(team : var Team) {.importc: "shmem_team_destroy", header : "<shmem.h>".}
+
+proc create(ctx : var Context, options : int) : int {.importc: "shmem_ctx_create", header : "<shmem.h>".}
+proc create(team : Team, ctx : var Context, options : int) : int {.importc: "shmem_ctx_create", header : "<shmem.h>".}
+proc destroy(ctx : var Context) : int {.importc: "shmem_ctx_destroy", header : "<shmem.h>".}
+proc get(ctx : Context, team : var Team) : int {.importc: "shmem_ctx_get_team", header : "<shmem.h>".}
+
+proc ini*() {.importc: "shmem_init", header: "<shmem.h>".}
+proc fin*() {.importc: "shmem_finalize", header: "<shmem.h>".}
+
+proc n_pes*() : uint32 {.importc: "shmem_n_pes", header: "<shmem.h>".}
+proc my_pe*() : uint32 {.importc: "shmem_my_pe", header: "<shmem.h>".}
+
+proc put*(dst : ptr UncheckedArray[int], src : ptr openarray[int], nelems : int, pe : int) {.importc: "shmem_int_put", header: "<shmem.h>".}
+proc put*(dst : ptr UncheckedArray[int32], src : ptr openarray[int32], nelems : int, pe : int) {.importc: "shmem_int_put", header: "<shmem.h>".} 
+proc put*(dst : ptr UncheckedArray[int64], src : ptr openarray[int64], nelems : int, pe : int) {.importc: "shmem_int_put", header: "<shmem.h>".} 
+
+proc put*(dst : ptr UncheckedArray[uint], src : ptr openarray[uint], nelems : int, pe : int) {.importc: "shmem_uint_put", header: "<shmem.h>".}
+proc put*(dst : ptr UncheckedArray[uint32], src : ptr openarray[uint32], nelems : int, pe : int) {.importc: "shmem_uint_put", header: "<shmem.h>".}
+proc put*(dst : ptr UncheckedArray[uint64], src : ptr openarray[uint64], nelems : int, pe : int) {.importc: "shmem_uint_put", header: "<shmem.h>".}
+
+proc put*(dst : ptr UncheckedArray[float32], src : ptr openarray[float32], nelems : int, pe : int) {.importc: "shmem_float_put", header: "<shmem.h>".} 
+proc put*(dst : ptr UncheckedArray[float64], src : ptr openarray[float64], nelems : int, pe : int) {.importc: "shmem_double_put", header: "<shmem.h>".} 
+
+macro put*(dst : ptr UncheckedArray[typed], src : ptr openarray[typed], nelems : int, pe : int) =
+    result = newStmtList()
+    result.add( bindSym"put", dst, src )
+
+proc get*(dst : ptr openarray[int], src : ptr UncheckedArray[int], nelems : int, pe : int) {.importc: "shmem_int_get", header: "<shmem.h>".} 
+proc get*(dst : ptr openarray[int32], src : ptr UncheckedArray[int32], nelems : int, pe : int) {.importc: "shmem_int_get", header: "<shmem.h>".} 
+proc get*(dst : ptr openarray[int64], src : ptr UncheckedArray[int64], nelems : int, pe : int) {.importc: "shmem_int_get", header: "<shmem.h>".} 
+
+proc get*(dst : ptr openarray[uint], src : ptr UncheckedArray[uint], nelems : int, pe : int) {.importc: "shmem_uint_get", header: "<shmem.h>".} 
+proc get*(dst : ptr openarray[uint32], src : ptr UncheckedArray[uint32], nelems : int, pe : int) {.importc: "shmem_uint_get", header: "<shmem.h>".} 
+proc get*(dst : ptr openarray[uint64], src : ptr UncheckedArray[uint64], nelems : int, pe : int) {.importc: "shmem_uint_get" , header: "<shmem.h>".} 
+
+proc get*(dst : ptr openarray[float32], src : ptr UncheckedArray[float32], nelems : int, pe : int) {.importc: "shmem_float_get", header: "<shmem.h>".} 
+proc get*(dst : ptr openarray[float64], src : ptr UncheckedArray[float64], nelems : int, pe : int) {.importc: "shmem_double_get", header: "<shmem.h>".} 
+
+macro get*(dst : ptr openarray[typed], src : ptr UncheckedArray[typed], nelems : int, pe : int) =
+    result = newStmtList()
+    result.add( bindSym"get", dst, src )
+
+proc nbput*(dst : ptr UncheckedArray[int], src : ptr openarray[int], nelems : int, pe : int) {.importc: "shmem_int_put_nbi", header: "<shmem.h>".}
+proc nbput*(dst : ptr UncheckedArray[int32], src : ptr openarray[int32], nelems : int, pe : int) {.importc: "shmem_int_put_nbi", header: "<shmem.h>".} 
+proc nbput*(dst : ptr UncheckedArray[int64], src : ptr openarray[int64], nelems : int, pe : int) {.importc: "shmem_int_put_nbi", header: "<shmem.h>".} 
+
+proc nbput*(dst : ptr UncheckedArray[uint], src : ptr openarray[uint], nelems : int, pe : int) {.importc: "shmem_uint_put_nbi", header: "<shmem.h>".}
+proc nbput*(dst : ptr UncheckedArray[uint32], src : ptr openarray[uint32], nelems : int, pe : int) {.importc: "shmem_uint_put_nbi", header: "<shmem.h>".}
+proc nbput*(dst : ptr UncheckedArray[uint64], src : ptr openarray[uint64], nelems : int, pe : int) {.importc: "shmem_uint_put_nbi", header: "<shmem.h>".}
+
+proc nbput*(dst : ptr UncheckedArray[float32], src : ptr openarray[float32], nelems : int, pe : int) {.importc: "shmem_float_put_nbi", header: "<shmem.h>".} 
+proc nbput*(dst : ptr UncheckedArray[float64], src : ptr openarray[float64], nelems : int, pe : int) {.importc: "shmem_double_put_nbi", header: "<shmem.h>".} 
+
+macro nbput*(dst : ptr UncheckedArray[typed], src : ptr openarray[typed], nelems : int, pe : int) =
+    result = newStmtList()
+    result.add( bindSym"nbput", dst, src )
+
+proc nbget*(dst : ptr openarray[int], src : ptr UncheckedArray[int], nelems : int, pe : int) {.importc: "shmem_int_get_nbi", header: "<shmem.h>".} 
+proc nbget*(dst : ptr openarray[int32], src : ptr UncheckedArray[int32], nelems : int, pe : int) {.importc: "shmem_int_get_nbi", header: "<shmem.h>".} 
+proc nbget*(dst : ptr openarray[int64], src : ptr UncheckedArray[int64], nelems : int, pe : int) {.importc: "shmem_int_get_nbi", header: "<shmem.h>".} 
+
+proc nbget*(dst : ptr openarray[uint], src : ptr UncheckedArray[uint], nelems : int, pe : int) {.importc: "shmem_uint_get_nbi", header: "<shmem.h>".} 
+proc nbget*(dst : ptr openarray[uint32], src : ptr UncheckedArray[uint32], nelems : int, pe : int) {.importc: "shmem_uint_get_nbi", header: "<shmem.h>".} 
+proc nbget*(dst : ptr openarray[uint64], src : ptr UncheckedArray[uint64], nelems : int, pe : int) {.importc: "shmem_uint_get_nbi" , header: "<shmem.h>".} 
+
+proc nbget*(dst : ptr openarray[float32], src : ptr UncheckedArray[float32], nelems : int, pe : int) {.importc: "shmem_float_get_nbi", header: "<shmem.h>".} 
+proc nbget*(dst : ptr openarray[float64], src : ptr UncheckedArray[float64], nelems : int, pe : int) {.importc: "shmem_double_get_nbi", header: "<shmem.h>".} 
+
+macro nbget*(dst : ptr openarray[typed], src : ptr UncheckedArray[typed], nelems : int, pe : int) =
+    result = newStmtList()
+    result.add( bindSym"nbget", dst, src )
+
+proc sync_all*() : int {.importc: "shmem_sync_all", header: "<shmem.h>".}
+proc barrier_all*() {.importc: "shmem_barrier_all", header: "<shmem.h>".}
+
+proc min_reduce*(team : Team, dst : ptr cshort, src : ptr cshort,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+proc min_reduce*(team : Team, dst : ptr cint, src : ptr cint,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+proc min_reduce*(team : Team, dst : ptr clong, src : ptr clong,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+
+proc min_reduce*(team : Team, dst : ptr cushort, src : ptr cushort,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+proc min_reduce*(team : Team, dst : ptr cuint, src : ptr cuint,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+proc min_reduce*(team : Team, dst : ptr culong, src : ptr culong,  nelems : int) : int {.importc: "shmem_int_min_reduce", header: "<shmem.h>".}
+
+proc min_reduce*(team : Team, dst : ptr cfloat, src : ptr cfloat,  nelems : int) : int {.importc: "shmem_float_min_reduce", header: "<shmem.h>".}
+proc min_reduce*(team : Team, dst : ptr cdouble, src : ptr cdouble,  nelems : int) : int {.importc: "shmem_double_min_reduce", header: "<shmem.h>".}
+
+#macro min_reduce*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int) : int =
+#    result = newStmtList()
+#    result.add( bindSym"min_reduce", team, src, dst, nelems)
+
+proc max_reduce*(team : Team, dst : ptr cshort, src : ptr cshort,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+proc max_reduce*(team : Team, dst : ptr cint, src : ptr cint,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+proc max_reduce*(team : Team, dst : ptr clong, src : ptr clong,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+
+proc max_reduce*(team : Team, dst : ptr cushort, src : ptr cushort,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+proc max_reduce*(team : Team, dst : ptr cuint, src : ptr cuint,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+proc max_reduce*(team : Team, dst : ptr culong, src : ptr culong,  nelems : int) : int {.importc: "shmem_int_max_reduce", header: "<shmem.h>".}
+
+proc max_reduce*(team : Team, dst : ptr cfloat, src : ptr cfloat,  nelems : int) : int {.importc: "shmem_float_max_reduce", header: "<shmem.h>".}
+proc max_reduce*(team : Team, dst : ptr cdouble, src : ptr cdouble,  nelems : int) : int {.importc: "shmem_double_max_reduce", header: "<shmem.h>".}
+
+#macro max_reduce*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int) : int =
+#    result = newStmtList()
+#    result.add( bindSym"max_reduce", team, src, dst, nelems)
+
+proc sum_reduce*(team : Team, dst : ptr cshort, src : ptr cshort,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+proc sum_reduce*(team : Team, dst : ptr cint, src : ptr cint,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+proc sum_reduce*(team : Team, dst : ptr clong, src : ptr clong,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+
+proc sum_reduce*(team : Team, dst : ptr cushort, src : ptr cushort,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+proc sum_reduce*(team : Team, dst : ptr cuint, src : ptr cuint,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+proc sum_reduce*(team : Team, dst : ptr culong, src : ptr culong,  nelems : int) : int {.importc: "shmem_int_sum_reduce", header: "<shmem.h>".}
+
+proc sum_reduce*(team : Team, dst : ptr cfloat, src : ptr cfloat,  nelems : int) : int {.importc: "shmem_float_sum_reduce", header: "<shmem.h>".}
+proc sum_reduce*(team : Team, dst : ptr cdouble, src : ptr cdouble,  nelems : int) : int {.importc: "shmem_double_sum_reduce", header: "<shmem.h>".}
+
+#macro sum_reduce*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int) : int =
+#    result = newStmtList()
+#    result.add( bindSym"sum_reduce", team, src, dst, nelems)
+
+proc prod_reduce*(team : Team, dst : ptr cshort, src : ptr cshort,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+proc prod_reduce*(team : Team, dst : ptr cint, src : ptr cint,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+proc prod_reduce*(team : Team, dst : ptr clong, src : ptr clong,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+
+proc prod_reduce*(team : Team, dst : ptr cushort, src : ptr cushort,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+proc prod_reduce*(team : Team, dst : ptr cuint, src : ptr cuint,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+proc prod_reduce*(team : Team, dst : ptr culong, src : ptr culong,  nelems : int) : int {.importc: "shmem_int_prod_reduce", header: "<shmem.h>".}
+
+proc prod_reduce*(team : Team, dst : ptr cfloat, src : ptr cfloat,  nelems : int) : int {.importc: "shmem_float_prod_reduce", header: "<shmem.h>".}
+proc prod_reduce*(team : Team, dst : ptr cdouble, src : ptr cdouble,  nelems : int) : int {.importc: "shmem_double_prod_reduce", header: "<shmem.h>".}
+
+#macro prod_reduce*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int) : int =
+#    result = newStmtList()
+#    result.add( bindSym"prod_reduce", team, src, dst, nelems)
+
+proc alltoall*(team : Team, dst : ptr int, src : ptr int,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+proc alltoall*(team : Team, dst : ptr int32, src : ptr int32,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+proc alltoall*(team : Team, dst : ptr int64, src : ptr int64,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+
+proc alltoall*(team : Team, dst : ptr uint, src : ptr uint,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+proc alltoall*(team : Team, dst : ptr uint32, src : ptr uint32,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+proc alltoall*(team : Team, dst : ptr uint64, src : ptr uint64,  nelems : int) : int {.importc: "shmem_int_alltoall", header: "<shmem.h>".}
+
+proc alltoall*(team : Team, dst : ptr float32, src : ptr float32,  nelems : int) : int {.importc: "shmem_float_alltoall", header: "<shmem.h>".}
+proc alltoall*(team : Team, dst : ptr float64, src : ptr float64,  nelems : int) : int {.importc: "shmem_double_alltoall", header: "<shmem.h>".}
+
+macro alltoall*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int) : int =
+    result = newStmtList()
+    result.add( bindSym"alltoall", team, src, dst, nelems)
+
+proc broadcast*(team : Team, dst : ptr int, src : ptr int,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+proc broadcast*(team : Team, dst : ptr int32, src : ptr int32,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+proc broadcast*(team : Team, dst : ptr int64, src : ptr int64,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+
+proc broadcast*(team : Team, dst : ptr uint, src : ptr uint,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+proc broadcast*(team : Team, dst : ptr uint32, src : ptr uint32,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+proc broadcast*(team : Team, dst : ptr uint64, src : ptr uint64,  nelems : int, root:int) : int {.importc: "shmem_int_broadcast", header: "<shmem.h>".}
+
+proc broadcast*(team : Team, dst : ptr float32, src : ptr float32,  nelems : int, root:int) : int {.importc: "shmem_float_broadcast", header: "<shmem.h>".}
+proc broadcast*(team : Team, dst : ptr float64, src : ptr float64,  nelems : int, root:int) : int {.importc: "shmem_double_broadcast", header: "<shmem.h>".}
+
+macro broadcast*(team : Team, dst : ptr typed, src : ptr typed,  nelems : int, root:int) : int =
+    result = newStmtList()
+    result.add( bindSym"broadcast", team, src, dst, nelems, root)
+
+proc alloc*[T : SomeNumber]( nelems : int) : ptr T {.importc: "shmem_malloc", header: "<shmem.h>".}
+proc free*[T : SomeNumber](arr : ptr T) {.importc: "shmem_free", header: "<shmem.h>".}
+
+proc fence*() {.importc: "shmem_fence", header: "<shmem.h>".}
+proc quiet*() {.importc: "shmem_quiet", header: "<shmem.h>".}
